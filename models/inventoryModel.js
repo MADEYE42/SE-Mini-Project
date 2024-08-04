cecc660d359dd929d1335f7437ee53a1313cba30
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-08-04 22:57:54 +0530

    Inventory Schema done

--- models/inventoryModel.js
+++ models/inventoryModel.js
@@ -1,5 +1,38 @@
 const mongoose = require('mongoose');
 const inventorySchema = new mongoose.Schema({
+    inventoryType:{
+        type:String,
+        required:[true,'Inventory type required'],
+        enum:['in','out']
+    },
+    bloodGroup:{
+        type:String,
+        requires:[true,'Blood Group required'],
+        enum:['O+','O-','AB+','AB-','A+','A-','B+','B-']
+    },
+    bloodQuantity:{
+        type:Number,
+        required:[true,"Blood quantity is required"]
+    },
+    organisation:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'organisation',
+        required:[true,'Organisaton is required']
+    },
+    hospital:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'users',
+        required:function(){
+            return this.inventoryType==='out'
+        }
+    },
+    donar:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'users',
+        required:function(){
+            return this.inventoryType==='in'
+        }
+    },
     
-});
+},{timestamps:true});
 module.exports = mongoose.model('Inventory',inventorySchema)
